698b8c7f479e34e6fc1c398d22c0ab173607f6d1
vivi10128 <vihajalan@gmail.com>
2026-06-21 14:42:20 +0530

    saved

--- script.js
+++ script.js
@@ -6,4 +6,63 @@ function updateTime() {
     setInterval(updateTime, 1000);         
 
 
+dragElement(document.getElementById("Welcome"));
 
+function dragElement(element) {
+    var initialX = 0;
+    var initialY = 0;
+    var currentX = 0;
+    var currentY = 0;
+
+    if (document.getElementById(element.id + "header")) {
+        document.getElementById(element.id + "header").onmousedown = startDragging;
+    } else {
+        element.onmousedown = startDragging;
+    }
+    function startDragging(e) {
+        e = e || window.event;
+        e.preventDefault();
+        initialX = e.clientX;
+        initialY = e.clientY;
+        document.onmouseup = stopDragging;
+        document.onmousemove = dragElement;
+    }
+
+    function dragElement(e) {
+        e = e || window.event;
+        e.preventDefault();
+        currentX = initialX - e.clientX;
+        currentY = initialY - e.clientY;
+        initialX = e.clientX;
+        initialY = e.clientY;
+        element.style.top = (element.offsetTop - currentY) + "px";
+        element.style.left = (element.offsetLeft - currentX) + "px";
+    }
+    
+    function stopDragging() {
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
+
+var welcomeScreen =
+document.querySelector("#Welcome")
+
+function closeWindow(element) {
+    element.style.display = "none";
+}
+
+function openWindow(element) {
+    element.style.display = "flex";
+}
+
+var welcomeScreenClose = 
+document.querySelector("#Welcomeclose")
+
+var welcomeScreenOpen =
+document.querySelector("#welcomeopen")
+
+welcomeScreenClose.addEventListener("click",
+    function() {
+        closeWindow(welcomeScreen);
+    });
