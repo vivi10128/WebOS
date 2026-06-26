13f979f1346610a7d7d6d73dc5d511e4838301ac
vivi10128 <vihajalan@gmail.com>
2026-06-26 12:04:25 +0530

    s

--- script.js
+++ script.js
@@ -67,3 +67,26 @@ notesscreenopen.addEventListener("click", function() {
 
 
 var selectedIcon = undefined
+
+function selectIcon(element) {
+    element.classList.add("selected");
+    selectedIcon = element
+}
+
+function deselectIcon(element) {
+    element.classList.remove("selected");
+    selectedIcon = undefined
+}
+
+function handleIconTap(element) {
+    if
+    (element.classList.contains("selected"))
+    { 
+        deselectIcon(element)
+        openWindow(window)
+    } else {
+        selectIcon(element)
+    }
+}
+
+handleIconTap(document.querySelector(#notes));
